a1fa85cd95442dda492bbc97b40eda3844abe5de
phat nguyen <phat.nguyen@terralogic.com>
2020-08-11 13:03:25 +0700

    intergration API

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,11 +1,11 @@
 const URL = {
-  SIGNUP: "/user/signup",
+  SIGNUP: "/v1/auth/register",
   SIGNIN: "/user/login",
   LOGOUT: "/user/logout",
   REFRESH_TOKEN: "/user/refresh-token",
   GET_MY_INFO: "/user/get-my-info",
 
-  PRODUCT: "/products",
+  PRODUCT: "/products"
 };
 
 export default URL;
