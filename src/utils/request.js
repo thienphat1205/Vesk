335f77bc1b0e07c785ce0163ab3ec95896a9dec5
phat nguyen <phat.nguyen@terralogic.com>
2020-08-11 11:11:24 +0700

    change file request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,17 @@
 import { getToken } from "../utils/token";
-import handleRefreshToken from "../utils/refreshToken";
+// import handleRefreshToken from "../utils/refreshToken";
 import { CONFIG } from "../config/config";
 
 const BASE_URL = CONFIG.BASE_URL;
 
-const request = async (url, method = "GET", payload, checkAuth) => {
+const request = async (url, method = "GET", payload, notAuth) => {
   const urlAPI = `${BASE_URL}${url}`;
   const payloadRequestRefreshToken = { urlAPI, method, payload };
   const { token } = await getToken();
 
   const body = payload ? JSON.stringify({ ...payload }) : undefined;
   let headers = { "Content-Type": "application/json" };
-  if (!checkAuth) {
+  if (!notAuth) {
     headers = {
       ...headers,
       Authorization: `Bearer ${token}`
@@ -26,38 +26,38 @@ const request = async (url, method = "GET", payload, checkAuth) => {
   const response = await callAPI(
     urlAPI,
     requestOptions,
-    checkAuth,
+    notAuth,
     payloadRequestRefreshToken
   );
   return response;
 };
 
-const callAPI = async (urlAPI, requestOptions, checkAuth, payloadCallBack) => {
-  const { urlAPI: url, method, payload } = payloadCallBack;
+const callAPI = async (urlAPI, requestOptions, notAuth, payloadCallBack) => {
+  // const { urlAPI: url, method, payload } = payloadCallBack;
   const response = await fetch(urlAPI, requestOptions);
   let data = await response.json();
-  if (data.statusCode === 401 && !checkAuth) {
-    const newDataToken = await handleRefreshToken();
-    if (newDataToken.statusCode === 200) {
-      const body = payload ? JSON.stringify({ ...payload }) : undefined;
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${newDataToken.accessToken}`
-      };
-      const requestOptions = {
-        method,
-        headers,
-        body
-      };
-      const response = await fetch(url, requestOptions);
-      data = await response.json();
-    } else {
-      data = {
-        statusCode: 401,
-        message: "Token Expried"
-      };
-    }
-  }
+  // if (data.statusCode === 401 && !notAuth) {
+  //   const newDataToken = await handleRefreshToken();
+  //   if (newDataToken.statusCode === 200) {
+  //     const body = payload ? JSON.stringify({ ...payload }) : undefined;
+  //     const headers = {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${newDataToken.accessToken}`
+  //     };
+  //     const requestOptions = {
+  //       method,
+  //       headers,
+  //       body
+  //     };
+  //     const response = await fetch(url, requestOptions);
+  //     data = await response.json();
+  //   } else {
+  //     data = {
+  //       statusCode: 401,
+  //       message: "Token Expried"
+  //     };
+  //   }
+  // }
   return data;
 };
 
